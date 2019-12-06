895ad1bb8f60eb93ce4fcdee51c5525125c31fa7
Paweł Dąbała <Pawel.Dabala@mediadirection.com.pl>
2019-12-06 08:41:17 +0100

    finish lesson 12.2.7

--- learning_pytest/twitter_test.py
+++ learning_pytest/twitter_test.py
@@ -1,5 +1,18 @@
 import pytest
+from _pytest import monkeypatch
 from twitter import Twitter
+
+
+class ResponseGetMock(object):
+    def json(self):
+        return {'avatar_url': 'test'}
+
+#nowy fixture który blokuje requesty
+# @pytest.fixture(autouse=True)
+# def no_requests(monkeypatch):
+#     korzystajac z monkeypatch usuneliśy request, zaden request nie zostanie wykorzystany z naszych
+#     testach.
+#     monkeypatch.delattr('requests.sessions.Session.request')
 
 
 @pytest.fixture
@@ -9,12 +22,23 @@ def backend(tmpdir):
     return temp_file
 
 
+@pytest.fixture(params=[None, 'python'])
+def username(request):
+    return request.param
+
+
 @pytest.fixture(params=['list', 'backend'], name='twitter')
-def fixture_twitter(backend, request):
+def fixture_twitter(backend, username, request, monkeypatch):
     if request.param == 'list':
-        twitter = Twitter()
+        twitter = Twitter(username=username)
     elif request.param == 'backend':
-        twitter = Twitter(backend=backend)
+        twitter = Twitter(backend=backend, username=username)
+
+    def monkey_return():
+        return 'test'
+
+    monkeypatch.setattr(twitter, 'get_user_avatar', monkey_return)
+
     return twitter
 
 
@@ -24,13 +48,14 @@ def test_twitter_initialization(twitter):
 
 def test_tweet_single_message(twitter):
     twitter.tweet('Test message')
-    assert twitter.tweets == ['Test message']
+    assert twitter.tweet_messages == ['Test message']
 
 
 def test_tweet_long_message(twitter):
     with pytest.raises(Exception):
         twitter.tweet('test' * 41)
-    assert twitter.tweets == []
+    assert twitter.tweet_messages == []
+
 
 def test_initialize_two_twitter_classes(backend):
     twitter1 = Twitter(backend=backend)
@@ -39,7 +64,7 @@ def test_initialize_two_twitter_classes(backend):
     twitter1.tweet('Test 1')
     twitter2.tweet('Test 2')
 
-    assert twitter2.tweets == ['Test 1', 'Test 2']
+    assert twitter2.tweet_messages == ['Test 1', 'Test 2']
 
 
 @pytest.mark.parametrize("message, expected", (
@@ -51,3 +76,11 @@ def test_initialize_two_twitter_classes(backend):
 ))
 def test_tweet_with_hashtag(twitter, message, expected):
     assert twitter.find_hashtags(message) == expected  # dorbra praktyka nazywanie
+
+
+def test_tweet_with_username(twitter):
+    if not twitter.username:
+        pytest.skip()
+
+    twitter.tweet('Test message')
+    assert twitter.tweets == [{'message': 'Test message', 'avatar': 'test'}]
---
+++ s
@@ -0,0 +1,23 @@
+[33mcommit a9ccb6e3dc24e4fd451891d8a6a10ae24c4e3f38[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m, [m[1;31morigin/master[m[33m, [m[1;31morigin/HEAD[m[33m)[m
+Author: Paweł Dąbała <Pawel.Dabala@mediadirection.com.pl>
+Date:   Wed Dec 4 07:37:44 2019 +0100
+
+    finished lesson 10.2.5
+
+[33mcommit 81bf374ac32d5cb9b21e6d3772c2649c5e6d2834[m
+Author: Paweł Dąbała <Pawel.Dabala@mediadirection.com.pl>
+Date:   Tue Dec 3 09:08:17 2019 +0100
+
+    finished lesson 9.2.3
+
+[33mcommit 002f6ceeef74e8149affbaea4363f8ea8c763e5a[m
+Author: Paweł Dąbała <Pawel.Dabala@mediadirection.com.pl>
+Date:   Mon Dec 2 08:14:52 2019 +0100
+
+    7.2.2 7:04
+
+[33mcommit 0c0ae6b982c2fdb115b2eab24066d4176fc6fd90[m
+Author: Pawel Dabala <pdabala@gmail.com>
+Date:   Mon Dec 2 06:24:51 2019 +0100
+
+    Initial commit
